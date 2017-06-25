2c4d81ea635fe4a930f2e5ca52a777b341565279
Omkar Moghe <omoghe@umich.edu>
2017-06-25 09:00:05 -0700

    minor styling on tag input

--- content_script.js
+++ content_script.js
@@ -1,5 +1,4 @@
 const db = chrome.storage.sync;
-debugger;
 let tagInput = null;
 function setupTagInput() {
   tagInput = document.createElement("input");
@@ -7,11 +6,11 @@ function setupTagInput() {
   tagInput.style.position = 'absolute';
   tagInput.style["z-index"] = 10000;
   tagInput.style.display = "none";
-  tagInput.style.border = "1px solid #ccc";
+  tagInput.style.border = "2px solid #ccc";
   tagInput.style["border-radius"] = "4px";
   tagInput.placeholder = "Enter a tag...";
   tagInput.style.width = "120px";
-  tagInput.style.padding = "2px";
+  tagInput.style.padding = "5px";
   document.body.appendChild(tagInput);
 }
 
@@ -44,7 +43,7 @@ function mouseupListener() {
     // There is text currently selected
     const rect = document.getSelection().getRangeAt(0).getBoundingClientRect();
     const topOffset = rect.top + document.body.scrollTop;
-    const leftOffset = rect.right + document.body.scrollLeft;
+    const leftOffset = rect.right + document.body.scrollLeft + 5;
     tagInput.style.top = `${topOffset}px`;
     tagInput.style.left = `${leftOffset}px`;
     tagInput.style.display = "block";
