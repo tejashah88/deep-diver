bdb0999efbdb78688d48ddb616a65bed8c3c8a08
Tejas Shah <tejashah88@gmail.com>
2017-06-25 12:19:16 -0700

    added multi-color highlighting

--- content_script.js
+++ content_script.js
@@ -2,6 +2,7 @@ const db = chrome.storage.sync;
 
 let tagInput = null;
 let lastTag = '';
+let actualLastTag = '';
 let currentTextSelection = "";
 
 window.onload = function() {
@@ -65,11 +66,45 @@ function sendUrlInfo(clickedOnURL) {
   });
 }
 
+const colors = ['yellow', 'cyan', 'orange', 'magenta', 'pink'];
+var colorIndex = 0;
+var tag2colorMap = {};
+
+function highlightSelection(tag) {
+  var highlightColor;
+  console.log(tag2colorMap);
+  if (tag in tag2colorMap) {
+    highlightColor = tag2colorMap[tag];
+  } else {
+    if (colorIndex == 4) colorIndex = 0;
+    tag2colorMap[tag] = colors[colorIndex];
+    colorIndex++;
+    highlightColor = tag2colorMap[tag];
+  };
+
+  if (window.getSelection) {
+    var range = window.getSelection().getRangeAt(0);
+    var span = document.createElement('span');
+    span.style.backgroundColor = highlightColor;
+    span.appendChild(range.extractContents());
+    range.insertNode(span);
+  }
+}
+
 function keyPressListener(e) {
   if (e.which == 13) {
     // Enter pressed
-    lastTag = e.target.value;
-    console.log('Adding a tag...', e.target.value);
+    console.log(e.target.value);
+
+    if (!e.target.value) {
+      lastTag = actualLastTag;
+    } else {
+      lastTag = e.target.value;
+      actualLastTag = lastTag;
+    }
+    
+    highlightSelection(lastTag);
+    console.log('Adding a tag...', lastTag);
 
     const messageRequest = {
       type: "note",
@@ -98,7 +133,7 @@ function mouseupListener() {
     tagInput.style.top = `${topOffset}px`;
     tagInput.style.left = `${leftOffset}px`;
     tagInput.style.display = "block";
-    tagInput.value = lastTag;
+    tagInput.value = actualLastTag;
   }
   else if (event.target !== tagInput) {
     tagInput.style.display = "none";
